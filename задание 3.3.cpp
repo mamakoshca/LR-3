0be5cd8eca41616b91827aa30cc9fa0a3dbb0c08
mamakoshca <enotartur5@gmail.com>
2023-10-04 13:34:25 +0700

    LR3
    LR3

---
+++ задание 3.3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+// Подключил бибилотеки
+using namespace std;
+
+/*
+Вариант 16 - вывести ln|x+sin bx| при x<=0,
+cos(bx*x) + 0.5*x при 0<x<=12
+и sqrt(a*a+x*x) при x>12
+*/
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	double a = 5.4, b = 5.3, x;
+	// Создал три переменные с типом double
+	cout << "Введите x: ";
+	cin >> x;
+	if (x <= 0) {
+		cout << log(abs(x + sin(x * b)));
+	}
+	else if (x > 12) {
+		cout << sqrt(a * a + x * x);
+	}
+	else {
+		cout << cos(b * x * x) + 0.5 * x;
+	}
+	/*
+	При помощи условных операторов
+	проверил значения переменных
+	*/
+}
+
+
---
+++ задание 3.4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <clocale>
+// Подключил билиотеки
+using namespace std;
+/*
+Вариант 16 - при помощи оператора swithc
+вывести день недели согласно его номеру
+*/
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int x;
+	// Создал переменную х
+	cout << "Введите x: ";
+	cin >> x;
+	switch (x) {
+		case 1:
+			cout << "Понедельник";
+			break;
+		case 2:
+			cout << "Вторник";
+			break;
+		case 3:
+			cout << "Среда";
+			break;
+		case 4:
+			cout << "Четверг";
+			break;
+		case 5:
+			cout << "Пятница";
+			break;
+		case 6:
+			cout << "Суббота";
+			break;
+		case 7:
+			cout << "Воскресенье";
+			break;
+	}
+	/*
+	При помощи оператора switch
+	вывел день недели, согласно его номеру
+	*/
+}
